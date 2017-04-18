a23acc804de0c114f732373603a5c9f9467c7137
maysgar <100346215@alumnos.uc3m.es>
2017-04-18 12:18:59 +0200

    2 triggers venga va

--- trigger2.sql
+++ trigger2.sql
@@ -1,19 +1,41 @@
 --b) Prevent non-valid visualizations regarding contracts (there cannot be taps in a date out of
 --contract). 
+-- 2 triggers: one for movies and one for series
 
-CREATE TRIGGER non_valid_visualizations
-AFTER INSERT OR UPDATE OF ++++++ ON contracts
+CREATE TRIGGER non_valid_visualizations_movies
+BEFORE INSERT OR UPDATE ON taps_movies
 FOR EACH ROW
 BEGIN
 DECLARE end_date DATE;
-				bad_date EXCEPTION;
-
-BEGIN 
+	start_date DATE;
+        bad_date EXCEPTION;
+BEGIN
 	SELECT enddate INTO end_date FROM contracts
-	 IF view_datetime > end_date
-	 THEN RAISE bad_date;
-	 END IF;
+	SELECT startdate INTO start_date FROM contracts
+	IF (:NEW.view_datetime > end_date) OR (:NEW.view_datetime > start_date)
+	THEN RAISE bad_date;
+	END IF;
 EXCEPTION
-	 WHEN bad_date
-				DBMS_OUTPUT.PUT_LINE('There cannot be taps in a date out of contract!');
+	 WHEN bad_date THEN
+	 DBMS_OUTPUT.PUT_LINE('There cannot be taps in a date out of contract!');
+END;
+
+
+
+CREATE TRIGGER non_valid_visualizations_series
+BEFORE INSERT OR UPDATE ON taps_series
+FOR EACH ROW
+BEGIN
+DECLARE end_date DATE;
+	start_date DATE;
+        bad_date EXCEPTION;
+BEGIN
+	SELECT enddate INTO end_date FROM contracts
+	SELECT startdate INTO start_date FROM contracts
+	IF (:NEW.view_datetime > end_date) OR (:NEW.view_datetime > start_date)
+	THEN RAISE bad_date;
+	END IF;
+EXCEPTION
+	 WHEN bad_date THEN
+	 DBMS_OUTPUT.PUT_LINE('There cannot be taps in a date out of contract!');
 END;
