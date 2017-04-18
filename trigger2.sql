2d62f2b632bd4908e90aa31242c1a87866357d4c
maysgar <100346215@alumnos.uc3m.es>
2017-04-18 14:08:09 +0200

    hacer cursores y queries

--- trigger2.sql
+++ trigger2.sql
@@ -5,18 +5,21 @@
 CREATE TRIGGER non_valid_tap_movies
 BEFORE INSERT OR UPDATE ON taps_movies
 FOR EACH ROW
-DECLARE end_date DATE;
-	start_date DATE;
-        bad_date EXCEPTION;
+DECLARE
+end_date DATE; --cursores
+start_date DATE;
+bad_date EXCEPTION;
 BEGIN
-	SELECT enddate INTO end_date FROM contracts;
-	SELECT startdate INTO start_date FROM contracts;
-	IF (:NEW.view_datetime > end_date) OR (:NEW.view_datetime > start_date)
-	THEN RAISE bad_date;
-	END IF;
+SELECT enddate INTO end_date
+FROM contracts;   -- from(querie) seleccionando startdate, enddate y haciendo join de contracts y taps_movies
+SELECT startdate INTO start_date
+FROM contracts;
+IF (:NEW.view_datetime > end_date) OR (:NEW.view_datetime < start_date)
+THEN RAISE bad_date;
+END IF;
 EXCEPTION
-	 WHEN bad_date THEN
-	 DBMS_OUTPUT.PUT_LINE('There cannot be taps in a date out of contract!');
+WHEN bad_date THEN
+DBMS_OUTPUT.PUT_LINE('There cannot be taps in a date out of contract!');
 END;
 /
 
@@ -31,7 +34,7 @@ DECLARE end_date DATE;
 BEGIN
 	SELECT enddate INTO end_date FROM contracts;
 	SELECT startdate INTO start_date FROM contracts;
-	IF (:NEW.view_datetime > end_date) OR (:NEW.view_datetime > start_date)
+	IF (:NEW.view_datetime > end_date) OR (:NEW.view_datetime < start_date)
 	THEN RAISE bad_date;
 	END IF;
 EXCEPTION
