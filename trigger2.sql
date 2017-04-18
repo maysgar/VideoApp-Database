a01fc2a169f929ed096caf974123e2b60e42b34b
maysgar <100346215@alumnos.uc3m.es>
2017-04-18 12:58:56 +0200

    los 2 triggers creados sin errores
    not tested yet

--- trigger2.sql
+++ trigger2.sql
@@ -2,15 +2,15 @@
 --contract). 
 -- 2 triggers: one for movies and one for series
 
-CREATE TRIGGER non_valid_visualizations_movies
+CREATE TRIGGER non_valid_tap_movies
 BEFORE INSERT OR UPDATE ON taps_movies
 FOR EACH ROW
 DECLARE end_date DATE;
 	start_date DATE;
         bad_date EXCEPTION;
 BEGIN
-	SELECT enddate INTO end_date FROM contracts
-	SELECT startdate INTO start_date FROM contracts
+	SELECT enddate INTO end_date FROM contracts;
+	SELECT startdate INTO start_date FROM contracts;
 	IF (:NEW.view_datetime > end_date) OR (:NEW.view_datetime > start_date)
 	THEN RAISE bad_date;
 	END IF;
@@ -18,18 +18,19 @@ EXCEPTION
 	 WHEN bad_date THEN
 	 DBMS_OUTPUT.PUT_LINE('There cannot be taps in a date out of contract!');
 END;
+/
 
 
 
-CREATE TRIGGER non_valid_visualizations_series
+CREATE TRIGGER non_valid_tap_series
 BEFORE INSERT OR UPDATE ON taps_series
 FOR EACH ROW
 DECLARE end_date DATE;
 	start_date DATE;
         bad_date EXCEPTION;
 BEGIN
-	SELECT enddate INTO end_date FROM contracts
-	SELECT startdate INTO start_date FROM contracts
+	SELECT enddate INTO end_date FROM contracts;
+	SELECT startdate INTO start_date FROM contracts;
 	IF (:NEW.view_datetime > end_date) OR (:NEW.view_datetime > start_date)
 	THEN RAISE bad_date;
 	END IF;
@@ -37,3 +38,4 @@ EXCEPTION
 	 WHEN bad_date THEN
 	 DBMS_OUTPUT.PUT_LINE('There cannot be taps in a date out of contract!');
 END;
+/
