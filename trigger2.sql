a90c3f9b3be06242e998086674ad66f229145984
maysgar <100346215@alumnos.uc3m.es>
2017-04-19 13:43:04 +0200

    FINAL

--- trigger2.sql
+++ trigger2.sql
@@ -1,44 +1,13 @@
---b) Prevent non-valid visualizations regarding contracts (there cannot be taps in a date out of
---contract). 
--- 2 triggers: one for movies and one for series
-
-CREATE TRIGGER non_valid_tap_movies
-BEFORE INSERT OR UPDATE ON taps_movies
+CREATE  OR REPLACE TRIGGER non_valid_tap_movies BEFORE INSERT ON taps_movies
 FOR EACH ROW
-DECLARE
-end_date DATE; --cursores
-start_date DATE;
-bad_date EXCEPTION;
-BEGIN
-SELECT enddate INTO end_date
-FROM contracts;   -- from(querie) seleccionando startdate, enddate y haciendo join de contracts y taps_movies
-SELECT startdate INTO start_date
-FROM contracts;
-IF (:NEW.view_datetime > end_date) OR (:NEW.view_datetime < start_date)
-THEN RAISE bad_date;
-END IF;
-EXCEPTION
-WHEN bad_date THEN
-DBMS_OUTPUT.PUT_LINE('There cannot be taps in a date out of contract!');
-END;
-/
-
-
-
-CREATE TRIGGER non_valid_tap_series
-BEFORE INSERT OR UPDATE ON taps_series
-FOR EACH ROW
-DECLARE end_date DATE;
-	start_date DATE;
-        bad_date EXCEPTION;
-BEGIN
-	SELECT enddate INTO end_date FROM contracts;
-	SELECT startdate INTO start_date FROM contracts;
-	IF (:NEW.view_datetime > end_date) OR (:NEW.view_datetime < start_date)
-	THEN RAISE bad_date;
-	END IF;
-EXCEPTION
-	 WHEN bad_date THEN
-	 DBMS_OUTPUT.PUT_LINE('There cannot be taps in a date out of contract!');
-END;
+	DECLARE
+		myContract contracts%ROWTYPE;
+	BEGIN
+		SELECT * INTO myContract
+			FROM contracts
+			WHERE contractId = :NEW.contractId	AND enddate IS NOT NULL;
+		IF (TO_DATE(:NEW.view_datetime, 'DD-MM-YYYY') > myContract.enddate OR TO_DATE(:NEW.view_datetime, 'DD-MM-YYYY') < myContract.startdate OR TO_DATE(:NEW.view_datetime, 'DD-MM-YYYY') > sysdate)
+		 THEN RAISE_APPLICATION_ERROR (-20001, 'There cannot be taps in a date out of contract!');
+	    END IF;
+END non_valid_tap_movies;
 /
