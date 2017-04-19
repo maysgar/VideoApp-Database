3c46aecd7017eb73f42b50688c7fc7904838ecad
maysgar <100346215@alumnos.uc3m.es>
2017-04-19 13:50:52 +0200

    FINAL

--- trigger2.sql
+++ trigger2.sql
@@ -1,4 +1,4 @@
-CREATE  OR REPLACE TRIGGER non_valid_tap_movies BEFORE INSERT ON taps_movies
+CREATE OR REPLACE TRIGGER non_valid_tap_movies BEFORE INSERT ON taps_movies
 FOR EACH ROW
 	DECLARE
 		myContract contracts%ROWTYPE;
@@ -10,4 +10,18 @@ FOR EACH ROW
 		 THEN RAISE_APPLICATION_ERROR (-20001, 'There cannot be taps in a date out of contract!');
 	    END IF;
 END non_valid_tap_movies;
+/
+
+CREATE OR REPLACE TRIGGER non_valid_tap_series BEFORE INSERT ON taps_series
+FOR EACH ROW
+	DECLARE
+		myContract contracts%ROWTYPE;
+	BEGIN
+		SELECT * INTO myContract
+			FROM contracts
+			WHERE contractId = :NEW.contractId	AND enddate IS NOT NULL;
+		IF (TO_DATE(:NEW.view_datetime, 'DD-MM-YYYY') > myContract.enddate OR TO_DATE(:NEW.view_datetime, 'DD-MM-YYYY') < myContract.startdate OR TO_DATE(:NEW.view_datetime, 'DD-MM-YYYY') > sysdate)
+		 THEN RAISE_APPLICATION_ERROR (-20001, 'There cannot be taps in a date out of contract!');
+	    END IF;
+END non_valid_tap_series;
 /
